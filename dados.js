8d5e9fbf614ba6a350de0690017149d1ccf30b18
Cristiane-Dev <michellecristiane39@gmail.com>
2024-09-07 23:03:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Plano Essencial",
+        descricao: "Antivirus em tempo real, Proteção para pagamentos online, Recursos de desempenho, Antiphishing, Firewall bidirecional, Otimização de desempenho, Gerenciamento de aplicativos.",
+        link: "https://www.kaspersky.com.br/standard",
+        tags: "essencial rastreamento online não autorizado proteção de webcam e microfone detecção de stalkerware"
+    },
+    {
+        titulo: "Plano Avançado",
+        descricao: "Antivírus em tempo real, Proteção para pagamentos online, Recursos de desempenho, VPN super rápida ilimitada, Verificador de vazamento de dados.",
+        link: "https://www.kaspersky.com.br/plus",
+        tags: "avançado anti-ransomware bloqueador de ataques de rede firewall bidirecional"
+    },
+    {
+        titulo: "Plano Premium",
+        descricao: "Antivírus em tempo real, Proteção para pagamentos online, Recursos de desempenho, VPN super rápida ilimitada, Verificador de vazamento de dados, Proteção de identidade, Verificação e remoção de vírus por especialistas",
+        link: "https://www.kaspersky.com.br/premium",
+        tags: " premium detecção de acesso remoto suporte administrativo remoto"
+    }
+];
